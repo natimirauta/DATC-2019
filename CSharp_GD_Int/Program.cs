08c8711b06559d1b6181cb1772736f72945a9635
Nati Mirauta <natimirauta@gmail.com>
2019-10-07 09:03:15 +0300

    Init csproj

---
+++ CSharp_GD_Int/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_GD_Int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
