9f31efa38b01a102c75148185be6adc81cad08b7
Nati Mirauta <natimirauta@gmail.com>
2019-10-07 09:39:30 +0300

    Added json

--- CSharp_GD_Int/Program.cs
+++ CSharp_GD_Int/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+using Google.Apis.Drive.v3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+
+
 
 namespace CSharp_GD_Int
 {
     class Program
     {
+
+        private static DriveService _service;
+        private static string _token;
+
+        static void init(){
+
+            string[] scopes = new string[]{
+
+                DriveService.Scope.Drive,
+                DriveService.Scope.DriveFile
+
+            };
+
+            var clientID = "210295401760-rg1cisrkke2csu3m3bpskmr26h2e9okl.apps.googleusercontent.com";
+            var clientSecret = "cztXJagKeSVv-XFvHOQNrW0H";
+
+            var credentials = GoogleWebAuthorizationBroker.AuthorizeAsync(
+
+                new ClientSecrets
+                {
+
+                    ClientId = clientID,
+                    ClientSecret = clientSecret
+
+                },
+                scopes,
+                Environment.UserName,
+                CancellationToken.None,
+                // new FileDataStore("Daimto.GoogleDrive.Auth.Store")
+                null
+
+            ).Result;
+
+            Console.WriteLine("Token is: " + credentials.Token.AccessToken);
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            init();
         }
+
     }
 }
